cf82a6ce8d091980fce0ab7a3c2a2a732b3eaf1c
luowei <luowei010101@gmail.com>
2014-07-03 09:35:19 +0800

    Initial Commit

---
+++ AngryBirds/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AngryBirds
+//
+//  Created by luowei on 14-7-3.
+//  Copyright (c) 2014年 rootls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
